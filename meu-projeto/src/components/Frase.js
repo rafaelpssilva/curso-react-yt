5cfa5bdb70f01a49d268311132b85ec648ecf241
Rafael Pereira <94644258+rafaelpssilva@users.noreply.github.com>
2022-03-10 16:43:28 -0300

    Update Frase.js

--- meu-projeto/src/components/Frase.js
+++ meu-projeto/src/components/Frase.js
@@ -3,7 +3,7 @@ import style from './Frase.module.css'
 function Frase() {
     return (
         <div className={style.fraseContainer}>
-            <p className={style.fraseContent}>Esté componente é uma frase</p>
+            <p className={style.fraseContent}>Esté componente é uma frase!</p>
         </div>
     )
 }
