69a2b3c230f3f747d9c6b6c9889ac54b76c31d59
Rafael Pereira <94644258+rafaelpssilva@users.noreply.github.com>
2022-03-10 16:29:41 -0300

    refatoração 2.0

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
+import SayMyName from './components/SayMyName'
+import Frase from './components/Frase'
+
+
 function App() {
+  const nome = "Rafael"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Quem sou eu??</h1>
+      <SayMyName nome={nome.toLowerCase()} idade={5 + 11} profissão="Programador" />
+      <Frase />
     </div>
   );
 }
